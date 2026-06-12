7b1a97974046c6e0e39b407b400b6b7f06f53a9c
asfar95 <asfar95@gmail.com>
2026-06-12 00:17:19 -0700

    fix: changed calculateTax to use percentage correctly (issue #20)

--- src/utils.js
+++ src/utils.js
@@ -1,34 +1,5 @@
-function calculateDiscount(price, discount) {
-  var result = price - (price * discount / 100);
-  return result;
-}
-
-function getUserData(users, id) {
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].id == id) {
-      return users[i];
-    }
-  }
-}
-
-function processItems(items) {
-  var results = [];
-  for (var i = 0; i < items.length; i++) {
-    var item = items[i];
-    var processed = {
-      id: item.id,
-      name: item.name,
-      value: item.value * 2
-    };
-    results.push(processed);
-  }
-  return results;
-}
-
-// BUG: taxRate is treated as a whole number instead of a percentage
-// calculateTax(100, 20) returns 2000 instead of 20
 function calculateTax(price, taxRate) {
-  return price * taxRate;
+  return price - (price * taxRate / 100);
 }
 
 module.exports = { calculateDiscount, getUserData, processItems, calculateTax };
